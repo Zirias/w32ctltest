55be3723e6f79448d172283f9136a3f219c5393f
Felix Palmen <felix@palmen-it.de>
2017-05-10 10:47:17 +0200

    windows forms reference

---
+++ winforms/demo.cs
@@ -0,0 +1,48 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace W32CtlTest
+{
+    public class Demo : Form
+    {
+        private FlowLayoutPanel panel;
+        private Button button;
+        private TextBox textBox;
+
+        public Demo() : base()
+        {
+            Text = "W32CtlTest Windows.Forms";
+            panel = new FlowLayoutPanel();
+
+            button = new Button();
+            button.Text = "test";
+            button.Click += (sender, args) =>
+            {
+                Close();
+            };
+            panel.Controls.Add(button);
+
+            textBox = new TextBox();
+            panel.Controls.Add(textBox);
+
+            Controls.Add(panel);
+        }
+
+        protected override Size DefaultSize
+        {
+            get
+            {
+                return new Size(480,180);
+            }
+        }
+
+        public static void Main(string[] argv)
+        {
+            if (argv.Length < 1 || argv[0] != "-s")
+            {
+                Application.EnableVisualStyles();
+            }
+            Application.Run(new Demo());
+        }
+    }
+}
